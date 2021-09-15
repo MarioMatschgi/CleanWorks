f9b4c7569045dc0140828aebc26eb37b11e5972e
Mario Elsnig <46848943+MarioMatschgi@users.noreply.github.com>
2021-09-15 22:00:03 +0200

    Refinements

--- src/app/components/toolbar/tb-nav/tb-nav.component.ts
+++ src/app/components/toolbar/tb-nav/tb-nav.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  Input,
+} from '@angular/core';
 import { Icons } from 'src/libraries/util/models/icons.model';
 import { GlobalVariablesService } from 'src/libraries/util/services/global-variables.service';
 import { RouterService } from 'src/libraries/util/services/router.service';
@@ -8,25 +13,32 @@ import { RouterService } from 'src/libraries/util/services/router.service';
   templateUrl: './tb-nav.component.html',
   styleUrls: ['./tb-nav.component.scss'],
 })
-export class TbNavComponent implements OnInit {
+export class TbNavComponent implements AfterViewInit {
   @Input() sidenav;
   items: { url?: string; urlArr?: string[]; content?: string; icon?: Icons }[];
 
   constructor(
     public gv: GlobalVariablesService,
-    public router: RouterService
+    public router: RouterService,
+    private cd: ChangeDetectorRef
   ) {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.items = [
-      {
-        url: 'dashboard',
-        icon: this.gv.Icons.home,
-      },
       { url: 'homework', content: 'Homework' },
       { url: 'subjects', content: 'Subjects' },
       { urlArr: ['me', 'grades'], content: 'Grades' },
       { url: 'appointments', content: 'Appointments' },
     ];
+    if (this.sidenav == null) {
+      this.items = [
+        {
+          url: 'dashboard',
+          icon: this.gv.Icons.home,
+        },
+        ...this.items,
+      ];
+    }
+    this.cd.detectChanges();
   }
 }
